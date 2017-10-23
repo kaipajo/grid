f128504eb28424d37e61f0489a453d40bf479b83
kaipajo <jone.kaipainen@capgemini.com>
2017-10-23 15:36:20 +0300

    Initial commit

---
+++ grid_v1.1.py
@@ -0,0 +1,27 @@
+import sys
+import ast
+
+def challenge():
+
+  dicts_from_file = []
+  with open('../dictionary.txt','r') as inf:
+    dict_from_file = ast.literal_eval(inf.read())
+
+  chal1 = dict_from_file[input('Give first grid challenge: ')]
+  chal2 = dict_from_file[input('Give second grid challenge: ')]
+  chal3 = dict_from_file[input('Give third grid challenge: ')]
+
+  print(chal1,chal2,chal3)
+  return play_again()
+
+def play_again():
+    while True:
+       answer = input('Do you want to rerun the program?:')
+       if answer.lower().startswith("n"):
+          print("ok, exiting")
+          sys.exit()
+       elif answer.lower().startswith("y"):
+          print("ok, running again")
+          return challenge()
+		  
+challenge()
